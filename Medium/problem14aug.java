c66b9692bcaeb5204e571e6f7a70a27447f6b3db
Samarjeet Shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2023-08-16 00:29:35 +0530

    Create problem14aug.java

---
+++ Medium/problem14aug.java
@@ -0,0 +1,36 @@
+/*
+Given an array A containing 2*N+2 positive numbers, out of which 2*N numbers exist in pairs whereas the other two number occur exactly once and are distinct. Find the other two numbers. Return in increasing order.
+
+Example 1:
+
+Input: 
+N = 2
+arr[] = {1, 2, 3, 2, 1, 4}
+Output:
+3 4 
+Explanation:
+3 and 4 occur exactly once.
+*/
+
+class Solution
+{
+public:
+   
+     vector<int> singleNumber(vector<int> nums)
+    {
+         vector<int> ans;
+         map<int,int> m;
+         
+         for(int x:nums){
+             m[x]++;
+         }
+         
+         for(auto x:m){
+             if(x.second==1){
+                 ans.push_back(x.first);
+             }
+         }
+         
+         return ans;
+    }
+};
